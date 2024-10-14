6c4dfbb4ff26eacc14e0b32ccb16773b34e9f864
joaquin <joaquin.torres@ext.konecta-group.com>
2024-10-13 19:17:52 -0500

    .

--- packages/people/src/index.ts
+++ packages/people/src/index.ts
@@ -9,21 +9,16 @@ import { Cradle, loadContainer } from './container';
 import PeoplePrivateController from './presentation/private/controllers/people.controller';
 import PeoplePublicController from './presentation/public/controllers/people.controller';
 
-const handler = async <T>(
-  event: any,
-  context: MiddyLambdaContext,
-  type: new (...args: any[]) => T,
-  method: keyof T
-) => {
-  const container = await loadContainer(),
-    middlewares = [container.cradle.errorInterceptor.use()],
-    controller = getPropertyValueByType<Cradle, T>(container.cradle, type);
+const handler =
+  <T>(type: new (...args: any[]) => T, method: keyof T) =>
+  async (event: any, context: MiddyLambdaContext) => {
+    const container = await loadContainer(),
+      controller = getPropertyValueByType<Cradle, T>(container.cradle, type);
 
-  return MiddyMiddleware.use(
-    (controller[method] as (...args: any[]) => any).bind(controller),
-    middlewares
-  )(event, context);
-};
+    return MiddyMiddleware.use((controller[method] as (...args: any[]) => any).bind(controller), [
+      container.cradle.errorInterceptor.use(),
+    ])(event, context);
+  };
 
 const privateMethods = getMethodsByPrototype<PeoplePrivateController>(
     PeoplePrivateController.prototype
@@ -32,27 +27,9 @@ const privateMethods = getMethodsByPrototype<PeoplePrivateController>(
 
 export = {
   // PRIVATE
-  search: (event: any, context: MiddyLambdaContext) =>
-    handler<PeoplePrivateController>(
-      event,
-      context,
-      PeoplePrivateController,
-      privateMethods.search
-    ),
-  create: (event: any, context: MiddyLambdaContext) =>
-    handler<PeoplePrivateController>(
-      event,
-      context,
-      PeoplePrivateController,
-      privateMethods.create
-    ),
+  search: handler<PeoplePrivateController>(PeoplePrivateController, privateMethods.search),
+  create: handler<PeoplePrivateController>(PeoplePrivateController, privateMethods.create),
 
   // PUBLIC
-  getSwapiAll: (event: any, context: MiddyLambdaContext) =>
-    handler<PeoplePublicController>(
-      event,
-      context,
-      PeoplePublicController,
-      publicMethods.getSwapiAll
-    ),
+  getSwapiAll: handler<PeoplePublicController>(PeoplePublicController, publicMethods.getSwapiAll),
 };
